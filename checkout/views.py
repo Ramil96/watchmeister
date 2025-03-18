66a704f178ceccdbe548208757b6b5462a7db50a
Ramil Tuazon <ramiltuazon@tests-Air.communityfibre.co.uk>
2025-03-18 02:52:00 +0000

    Add stripe

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51R3pGaBTORn4q2dgV0AqTIyy1eevSDv4omVvWYH5RsA91PB8p4QnS5PNBinLRkmkRS7RusAizvLdYs27gka8h2Qt00ABjB0JaH',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
