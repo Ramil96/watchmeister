998f2b5b574249f7ec4093ce945f729f897e7ce6
Ramil Tuazon <ramiltuazon@tests-Air.communityfibre.co.uk>
2025-03-24 19:37:18 +0000

    edit product and custom widget completion

--- products/models.py
+++ products/models.py
@@ -5,13 +5,13 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
-    
+
     def get_friendly_name(self):
         return self.friendly_name
 
@@ -29,4 +29,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
