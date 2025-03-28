f617a6d52bbdb0573d1abe0e13c95736f629a0ed
Ramil Tuazon <ramiltuazon@tests-Air.communityfibre.co.uk>
2025-03-28 00:42:33 +0000

    code: fix storages typo

--- watchmeister/settings.py
+++ watchmeister/settings.py
@@ -198,7 +198,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazon.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custmo_storages.StaticStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
